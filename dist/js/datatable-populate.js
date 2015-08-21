f88d89e5f4705c47dcf15386887f515cd22e2594
lopespontesleonardo@gmail.com <lopespontesleonardo@gmail.com>
2015-08-21 18:24:37 -0300

    ...

--- dist/js/datatable-populate.js
+++ dist/js/datatable-populate.js
@@ -1,30 +1,68 @@
 
-var component = angular.module('datatable-populate', ['datatables']);
-
-component.directive('dtPopulate', function() {
-	return {
-		restrict: 'A',
-		replace: false,
-		transclude: true,
-		link: function($scope, $element, $attrs){
-			$element.dataTable({
-				aaSorting: [],
-				bPaginate: true,
-				data: $scope.produtos,
-				columns: [{title: "Nome", data: "nome"},
-				          {title: "Descricao", data: "descricao"}],
-				oLanguage:{
-					sLoadingRecords: "Processando",
-					sZeroRecords:"Nenhum registro encontrado...",
-					oPaginate: {                                         
-						sFirst:    "Primeira",
-						sPrevious: "Anterior",
-						sNext:     "Seguinte",
-						sLast:     "Última"   
-					},
-				},
-				sDom: 'lrtip'
-			});
-		}
-	}
-});
+angular.module('datatable-populate', [])
+       .constant('MODULE_VERSION', '0.0.1')
+       .value('defaults', {
+    	   
+       })
+       .factory('DATATABLEFACTORY', function(){})
+       .directive('dtPopulate', function(){
+			return{
+				link: function($scope, $element, $attrs){
+					
+					var columns = $.map($element.find('th'), function(th){
+						if(th && $(th).attr("dt-data")){
+							return {title: $(th).text(), data: $(th).attr("dt-data")};
+						}
+					});
+					
+					function addCRUDButons(row){
+						var edit       = $(document.createElement('a'));
+						var remove     = $(document.createElement('a'));
+						var spanEdit   = $(document.createElement('span'));
+						var spanRemove = $(document.createElement('span'));
+						var td         = $(document.createElement('td'));
+						
+						$(spanEdit).addClass('glyphicon glyphicon-pencil');
+						$(spanRemove).addClass('glyphicon glyphicon-trash');
+						
+						$(spanEdit).attr('aria-hidden', 'true');
+						$(spanRemove).attr('aria-hidden', 'true');
+						
+						$(edit).addClass('btn btn-warning btn-sm');
+						$(remove).addClass('btn btn-danger btn-sm');
+						
+						$(edit).attr("role", "button");
+						$(remove).attr("role", "button");
+						
+						$(edit).append(spanEdit);
+						$(remove).append(spanRemove);
+						
+						$(td).append(edit);
+						$(td).append(remove);
+						$(td).addClass('align-center');
+						
+						$(row).append(td);
+					}
+					
+					var table = $element.DataTable({
+						data: $scope.$eval($attrs.dtData),
+						sorting: [],
+						sDom: 'rtip',
+						columns:columns,
+						createdRow: function(row, data, index){
+							if($attrs.dtCrudtable){
+								addCRUDButons(row);
+							}
+						}
+					});
+					
+					$scope.$watch($attrs.dtData, function(value){
+						var val = value || null;
+						/*if(val){
+							table.clear();
+							table.row.add($scope.$eval($attrs.dtData)).draw();
+						}*/
+					});
+				}
+			}
+	});
--- js/datatable-populate.js
+++ js/datatable-populate.js
@@ -1,30 +1,94 @@
+angular.module('datatable-populate', [])
+       .constant('MODULE_VERSION', '0.0.1')
+       .value('defaults', {
 
-var component = angular.module('datatable-populate', ['datatables']);
+       })
+       .provider('TableOptions', function TableOptionsProvider(){
+    	   this.options = {
+				sorting: [],
+				sDom: 'rtip'
+			};
 
-component.directive('dtPopulate', function() {
-	return {
-		restrict: 'A',
-		replace: false,
-		transclude: true,
-		link: function($scope, $element, $attrs){
-			$element.dataTable({
-				aaSorting: [],
-				bPaginate: true,
-				data: $scope.produtos,
-				columns: [{title: "Nome", data: "nome"},
-				          {title: "Descricao", data: "descricao"}],
-				oLanguage:{
-					sLoadingRecords: "Processando",
-					sZeroRecords:"Nenhum registro encontrado...",
-					oPaginate: {                                         
-						sFirst:    "Primeira",
-						sPrevious: "Anterior",
-						sNext:     "Seguinte",
-						sLast:     "Última"   
-					},
-				},
-				sDom: 'lrtip'
-			});
-		}
-	}
-});
+    	   this.setOptions = function(options) {
+    	        if (!angular.isObject(options)) throw new Error("Options should be an object!");
+    	        this.options = angular.extend({}, this.options, options);
+    	    };
+
+    	   this.$get = function(){
+    		   return this.options;
+    	   };
+       })
+       .directive('dtPopulate', ['TableOptions', function(TableOptions){
+			return{
+				link: function($scope, $element, $attrs){
+					var columns = $.map($element.find('th'), function(th){
+						if(th && $(th).attr("dt-data")){
+							return {title: $(th).text(), data: $(th).attr("dt-data")};
+						}
+					});
+
+					function addCRUDButons(row, data){
+						var edit       = $(document.createElement('a'));
+						var remove     = $(document.createElement('a'));
+						var spanEdit   = $(document.createElement('span'));
+						var spanRemove = $(document.createElement('span'));
+						var td         = $(document.createElement('td'));
+
+						$(spanEdit).addClass('glyphicon glyphicon-pencil');
+						$(spanRemove).addClass('glyphicon glyphicon-trash');
+
+						$(spanEdit).attr('aria-hidden', 'true');
+						$(spanRemove).attr('aria-hidden', 'true');
+
+						$(edit).addClass('btn btn-warning btn-sm');
+						$(remove).addClass('btn btn-danger btn-sm');
+
+						$(edit).attr("role", "button");
+						$(remove).attr("role", "button");
+
+						edit.on('click', function(){
+							if($scope.update){
+								$scope.update(data);
+							}
+						});
+
+						remove.on('click', function(){
+							if($scope.delete){
+								$scope.delete(data);
+							}
+						});
+
+						$(edit).append(spanEdit);
+						$(remove).append(spanRemove);
+
+						$(td).append(edit);
+						$(td).append(remove);
+						$(td).addClass('align-center');
+
+						$(row).append(td);
+					}
+
+					var defaultOptions = {
+						data: $scope.$eval($attrs.dtData),
+						columns:columns,
+						createdRow: function(row, data, index){
+							if($attrs.dtCrudtable){
+								addCRUDButons(row, data);
+							}
+						}	
+					};
+
+					var options = angular.extend({}, defaultOptions, TableOptions);
+
+					var table = $element.DataTable(options);
+
+					$scope.$watch($attrs.dtData, function(value){
+						var val = value || null;
+						if(val){
+							table.clear();
+							table.rows.add($scope.$eval($attrs.dtData)).draw();
+						}
+					});
+				}
+			}
+	}]); 
